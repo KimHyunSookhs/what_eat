baae3eefc20b689673abe31df3c206f2ab335810
unknown <rlagustn070@naver.com>
2025-06-11 17:07:56 +0900

    feat : map_screen 적용

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,21 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
+val flutterGoogleMapKey: String = localProperties.getProperty("flutter.GoogleMapKey") ?: ""
+
 
 android {
     namespace = "com.whateat.what_eat"
@@ -28,6 +40,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        resValue("string", "GOOGLE_MAP_KEY", flutterGoogleMapKey)
     }
 
     buildTypes {
