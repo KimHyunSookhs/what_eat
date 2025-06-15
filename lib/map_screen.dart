50943ce61e78ab0d3c1d9a61942adad499520af8
unknown <rlagustn070@naver.com>
2025-06-15 13:38:13 +0900

    feat : 현재위치 기반으로 위치 불러오도록 추가

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,18 +10,59 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
- GoogleMapController? mapController;
- final LatLng _center = const LatLng(37.5665, 126.9780);
+  GoogleMapController? mapController;
+  LocationData? _currentLocation;
+  final Location location = Location();
 
- void _onMapCreated(GoogleMapController controller){
-   mapController = controller;
- }
+  @override
+  void initState() {
+    super.initState();
+    _fetchCurrentLocation();
+  }
+
+  Future<void> _fetchCurrentLocation() async {
+    final locationData = await location.getLocation();
+    setState(() {
+      _currentLocation = locationData;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    if (_currentLocation != null) {
+      mapController!.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(
+              _currentLocation!.latitude!,
+              _currentLocation!.longitude!,
+            ),
+            zoom: 17.0,
+          ),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Map'),
-      ),body:  GoogleMap(initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),onMapCreated: _onMapCreated,),
+      appBar: AppBar(title: Text('Google Map')),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            _currentLocation!.latitude!,
+            _currentLocation!.longitude!,
+          ),
+          zoom: 4.0,
+        ),
+        onMapCreated: _onMapCreated,
+        //현재 위치 점으로 표시
+        myLocationEnabled: true,
+        //현재 위치로 카메라 이동 버튼
+        myLocationButtonEnabled: true,
+      ),
     );
   }
 }
